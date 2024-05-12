2eaca1ff75b4a608fbc2a2ac10655731809a67ce
JonasGMadsen <Jonas6070@hotmail.com>
2024-05-12 05:17:23 +0200

    Testing

--- app/src/main/java/com/example/composetesting/MainActivity.kt
+++ app/src/main/java/com/example/composetesting/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetesting.ui.theme.ComposeTestingTheme
 
@@ -28,11 +31,17 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
-    }
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Picture",
+        )
 
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
@@ -42,3 +51,4 @@ fun PreviewMessageCard() {
         msg = Message("John", "This is Jetpack Compose")
     )
 }
+
